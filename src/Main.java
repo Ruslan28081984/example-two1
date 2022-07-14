ef0ee9ad01196b8d235d2407b0e58a7df0847b4b
Ruslan Mustafin <89201828020@mail.ru>
2022-07-14 14:23:58 +0300

    второй комит. Пробую запушить на гитхаб

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      // задание 1:
+        int dog = 7;
+        byte cat = 1;
+        short elephant = 4;
+        long mouse = 300L;
+        double weightSugar = 15.5;
+        float weightCat = 7.5f;
+        char bar = 35;
+       // boolean dogIsAdult = dog > 7;
+
+        //System.out.println(bar);
+
+
+        //  Задание 2:
+         double boxer1 = 78.2;
+         double boxer2 = 82.7;
+         double weightAllBoxers = boxer1 + boxer2;
+         double weightDifference = boxer2 - boxer1;
+        System.out.println(" Общий вес боксеров " + weightAllBoxers);
+        System.out.println(" Разница в вее боксеров "+ weightDifference);
+
+
+        // Задание 3:
+
+        double weightBananas = 5 * 80;
+        double weightMilk = 200 * 1.05;
+        double weightIceCream = 2 * 100;
+        double weightEgg = 4 * 0.7;
+        double weightAllBreakfast = weightBananas + weightMilk + weightIceCream + weightEgg;
+        int grPerKg = 1000;
+        double weightKg = weightAllBreakfast / grPerKg;
+        System.out.println( " Общий вес завтрака спотрсмена соствил : " + weightKg + " кг");
+
+
+        //
+
+
+
+
+
+
+
+
     }
+
+
 }
