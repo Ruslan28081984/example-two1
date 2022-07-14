5e0498be679b68296a11a5957df29802060602a7
Ruslan Mustafin <89201828020@mail.ru>
2022-07-14 20:33:49 +0300

    Домашнее задание - 5 задач выполнил. Прошу проверить)).

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,53 @@ public class Main {
         System.out.println( " Общий вес завтрака спотрсмена соствил : " + weightKg + " кг");
 
 
-        //
+        // Задание 4:
+
+        int weightSportsmen = 7000;  // перевес спортсмена
+        int weightDay250 = 250;
+        int weightDay500 = 500;
+        int scoreDay250 = weightSportsmen / weightDay250;
+        int scoreDay500 = weightSportsmen / weightDay500;
+        int averageValue = ( scoreDay250 + scoreDay500) / 2;
+        System.out.println( " При сбросе в весе 250 грам в день, спортсмену понадобиться: " + scoreDay250);
+        System.out.println( " При сбросе в весе 500 грам в день, спортсмену понадобиться: " + scoreDay500);
+        System.out.println( " Среднее количесво дней спортсмену понадобиться: " + averageValue);
+
+
+        // Задание 5:
+
+        double MashaMoneyMonth = 67760;                                  // зарплата в месяц
+        double MashaMoneyYear = MashaMoneyMonth * 12;                    // зарплата в год без повышения
+        double MashaMoneyMonthUp = MashaMoneyMonth * 1.1;                // зарплата в месяц после повышения
+        double MashaMoneyYearUp = MashaMoneyMonthUp * 12;                // зарплата в год после повышения на 10 %
+        double MashaDifferenceYear = MashaMoneyYearUp - MashaMoneyYear;  // разница в год после повышения
+        System.out.println("Маша теперь получает " + MashaMoneyMonthUp + " рублей. Годовой доход вырос на "
+                + MashaDifferenceYear + " рублей");
+
+        double DenisMoneyMonth = 83690;                                  // зарплата в месяц
+        double DenisMoneyYear = DenisMoneyMonth * 12;                    // зарплата в год без повышения
+        double DenisMoneyMonthUp = DenisMoneyMonth * 1.1;                // зарплата в месяц после повышения
+        double DenisMoneyYearUp = DenisMoneyMonthUp * 12;                // зарплата в год после повышения на 10 %
+        double DenisDifferenceYear = DenisMoneyYearUp - DenisMoneyYear;  // разница в год после повышения
+        System.out.println("Денис теперь получает " + DenisMoneyMonthUp + " рублей. Годовой доход вырос на "
+                + DenisDifferenceYear + " рублей");
+
+
+        double KristinaMoneyMonth = 76230;                                        // зарплата в месяц
+        double KristinaMoneyYear = KristinaMoneyMonth * 12;                       // зарплата в год без повышения
+        double KristinaMoneyMonthUp = KristinaMoneyMonth * 1.1;                   // зарплата в месяц после повышения
+        double KristinaMoneyYearUp = KristinaMoneyMonthUp * 12;                   // зарплата в год после повышения на 10 %
+        double KristinaDifferenceYear = KristinaMoneyYearUp - KristinaMoneyYear;  // разница в год после повышения
+        System.out.println("Маша теперь получает " + KristinaMoneyMonthUp + " рублей. Годовой доход вырос на "
+                + KristinaDifferenceYear + " рублей");
+
+
+
+
+
+
+
+
 
 
 
